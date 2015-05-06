bd5fa409648d557eaa6ec833b067e1c039a9a518
Al Poole <al@Jerusalem.home>
2015-05-06 14:03:27 +0000

    wtfish?

--- gnuImage.c
+++ gnuImage.c
@@ -18,11 +18,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/select.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/time.h>
-#include <sys/select.h>
-#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
